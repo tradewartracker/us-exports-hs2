3324d42036c28e189d57816bf54eed4270b93edd
mikew <waugh.e.michael@gmail.com>
2022-12-08 11:11:21 -0500

    october update

--- main-exports-hs2.py
+++ main-exports-hs2.py
@@ -24,8 +24,8 @@ from bokeh.transform import factor_cmap
 # https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
 #
 
-final_month = 12
-final_year = 2022
+final_month = 1
+final_year = 2023
 
 background = "#ffffff"
 
